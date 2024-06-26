7509deb6ce0681dd76eaa5f61f49839709f1f64f
Ana Carolina Cornélio <carolinadscornelio@gmail.com>
2024-06-26 15:29:47 -0300

    Finalizadoo

--- app.py
+++ app.py
@@ -3,40 +3,42 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pages = int(request.form['pages'])
-        days = int(request.form['days'])
-        start_day = int(request.form['startDay'])
-        start_month = int(request.form['startMonth'])
-        
+        pags = int(request.form['pages'])
+        dias = int(request.form['days'])
+        comeco = int(request.form['startDay'])
+        mes = int(request.form['startMonth'])
+
         today = datetime.today()
-        start_date = datetime(today.year, start_month, start_day)
-        end_date = start_date + timedelta(days=days-1)
-        
-        pags_dia = (pages // days) + (1 if pages % days else 0)
-        
-        reading_schedule = []
-        current_page = 0
-        for d in range(days):
-            current_date = start_date + timedelta(days=d)
-            current_page += pags_dia
-            if current_page > pages:
-                current_page = pages
-            reading_schedule.append({
-                'date': current_date.strftime('%d/%m/%Y'),
-                'page': current_page
+        inicio = datetime(today.year, mes, comeco)
+        final = inicio + timedelta(days=dias-1)
+
+        pags_dia = (pags // dias) + (1 if pags % dias else 0)
+
+        lista = []
+        pag_atual = 0
+        for d in range(dias):
+            data_atual = inicio + timedelta(days=d)
+            pag_atual += pags_dia
+            if pag_atual > pags:
+                pag_atual = pags
+            lista.append({
+                'date': data_atual.strftime('%d/%m/%Y'),
+                'page': pag_atual
             })
-        
-        return render_template('index.html', 
-                               start_date=start_date.strftime('%d/%m/%Y'), 
-                               end_date=end_date.strftime('%d/%m/%Y'), 
-                               days=days, 
-                               pags_dia=pags_dia, 
-                               reading_schedule=reading_schedule)
-    
+
+        return render_template('table.html',
+                               inicio=inicio.strftime('%d/%m/%Y'),
+                               final=final.strftime('%d/%m/%Y'),
+                               dias=dias,
+                               pags_dia=pags_dia,
+                               lista=lista)
+
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
